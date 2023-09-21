364575c656fe3d770c93aa9882c8a6ac4c9dec18
Cattuong Le <lcattuong@gmail.com>
2023-09-20 23:45:30 -0400

    Design 2,3,5 created

---
+++ pointcp/design5/PointCP2mod.java
@@ -0,0 +1,143 @@
+// This file contains material supporting section 2.9 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at http://www.site.uottawa.ca/school/research/lloseng/
+
+/**
+ * This class contains instances of coordinates in either polar or
+ * cartesian format.  It also provides the utilities to convert
+ * them into the other type. It is not an optimal design, it is used
+ * only to illustrate some design issues.
+ *
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Dr Timothy C. Lethbridge
+ * @version July 2000
+ */
+public class PointCP2mod extends PointCP5
+{	
+  
+  //Constructors ******************************************************
+
+  /**
+   * Constructs a coordinate object, with a type identifier.
+   */
+  public PointCP2mod(char type, double xOrRho, double yOrTheta)
+  {
+    if(type != 'C' && type != 'P'){
+      throw new IllegalArgumentException();
+    }
+    else if(type == 'P'){
+      this.xOrRho = xOrRho;
+      this.yOrTheta = yOrTheta;
+    }
+    else {//if coordinates are cartesian
+      // convert cartesian to polar and store
+      this.xOrRho = (Math.sqrt(Math.pow(xOrRho, 2) + Math.pow(yOrTheta, 2)));
+      this.yOrTheta = (Math.toDegrees(Math.atan2(yOrTheta, xOrRho)));
+    }
+    typeCoord = type;
+  }
+
+  
+  //Instance methods **************************************************
+ 
+ 
+  public double getX()
+  { 
+    return (Math.cos(Math.toRadians(yOrTheta)) * xOrRho);
+  }
+  
+  public double getY()
+  {
+    return (Math.sin(Math.toRadians(yOrTheta)) * xOrRho);
+  }
+  
+  public double getRho()
+  {
+    return xOrRho;
+  }
+  
+  public double getTheta()
+  {
+    return yOrTheta;
+  }
+  
+	
+  /**
+   * Converts Cartesian coordinates to Polar coordinates.
+   */
+  public void convertStorageToPolar()
+  {
+    if(typeCoord != 'P')
+    {
+      //Calculate RHO and THETA
+      double temp = getRho();
+      yOrTheta = getTheta();
+      xOrRho = temp;
+      
+      typeCoord = 'P';  //Change coord type identifier
+    }
+  }
+
+  /**
+   * Converts Polar coordinates to Cartesian coordinates.
+   */
+  public void convertStorageToCartesian()
+  {
+    if(typeCoord != 'C')
+    {
+      //Calculate X and Y
+      double temp = getX();
+      yOrTheta = getY();
+      xOrRho = temp;
+   
+      typeCoord = 'C';  //Change coord type identifier
+    }
+  }
+  
+  /**
+   * Calculates the distance in between two points using the Pythagorean
+   * theorem  (C ^ 2 = A ^ 2 + B ^ 2). Not needed until E2.30.
+   *
+   * @param pointA The first point.
+   * @param pointB The second point.
+   * @return The distance between the two points.
+   */
+  public double getDistance(PointCP5 pointB)
+  {
+    // Obtain differences in X and Y, sign is not important as these values
+    // will be squared later.
+    double deltaX = getX() - pointB.getX();
+    double deltaY = getY() - pointB.getY();
+    
+    return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+  }
+
+  /**
+   * Rotates the specified point by the specified number of degrees.
+   * Not required until E2.30
+   *
+   * @param point The point to rotate
+   * @param rotation The number of degrees to rotate the point.
+   * @return The rotated image of the original point.
+   */
+  public PointCP2mod rotatePoint(double rotation)
+  {
+    double radRotation = Math.toRadians(rotation);
+    double X = getX();
+    double Y = getY();
+        
+    return new PointCP2mod('C',
+      (Math.cos(radRotation) * X) - (Math.sin(radRotation) * Y),
+      (Math.sin(radRotation) * X) + (Math.cos(radRotation) * Y));
+  }
+
+  /**
+   * Returns information about the coordinates.
+   *
+   * @return A String containing information about the coordinates.
+   */
+  public String toString()
+  {
+    return "Stored as Polar  (" + getRho() + "," + getTheta() + ")";
+  }
+}
---
+++ pointcp/design5/PointCP5.java
@@ -0,0 +1,35 @@
+// This file contains material supporting section 2.9 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at http://www.site.uottawa.ca/school/research/lloseng/
+
+/**
+ * This class contains instances of coordinates in either polar or
+ * cartesian format.  It also provides the utilities to convert
+ * them into the other type. It is not an optimal design, it is used
+ * only to illustrate some design issues.
+ *
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Dr Timothy C. Lethbridge
+ * @version July 2000
+ */
+public abstract class PointCP5
+{
+    public abstract double getX();
+    public abstract double getY();
+    public abstract double getRho();
+    public abstract double getTheta();
+
+    protected char typeCoord;
+    protected double xOrRho;
+    protected double yOrTheta;
+
+    public abstract double getDistance(PointCP5 pointB);
+
+
+  public String toString()
+  {
+    return "Stored as " + (typeCoord == 'C' 
+       ? "Cartesian  (" + getX() + "," + getY() + ")"
+       : "Polar [" + getRho() + "," + getTheta() + "]") + "\n";
+  }
+}
